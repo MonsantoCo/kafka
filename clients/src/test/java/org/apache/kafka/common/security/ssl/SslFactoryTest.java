e637e9c96384639f00883e897ab760d929223597
Allen Xiang <allen.xiang@monsanto.com>
2017-04-13 08:17:32 -0500

    Update logging.

--- clients/src/test/java/org/apache/kafka/common/security/ssl/SslFactoryTest.java
+++ clients/src/test/java/org/apache/kafka/common/security/ssl/SslFactoryTest.java
@@ -12,19 +12,26 @@
  */
 package org.apache.kafka.common.security.ssl;
 
-import java.io.File;
-import java.util.Map;
-
-import javax.net.ssl.SSLEngine;
-
-import org.apache.kafka.test.TestSslUtils;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.config.types.Password;
 import org.apache.kafka.common.network.Mode;
+import org.apache.kafka.test.TestSslUtils;
 import org.junit.Test;
 
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.File;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * A set of tests for the selector over ssl. These use a test harness that runs a simple socket server that echos back responses.
@@ -46,6 +53,20 @@ public class SslFactoryTest {
     }
 
     @Test
+    public void testSslFactoryWithoutPasswordConfiguration() throws Exception {
+        File trustStoreFile = File.createTempFile("truststore", ".jks");
+        Map<String, Object> serverSslConfig = TestSslUtils.createSslConfig(false, true, Mode.SERVER, trustStoreFile, "server");
+        // unset the password
+        serverSslConfig.remove(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG);
+        SslFactory sslFactory = new SslFactory(Mode.SERVER);
+        try {
+            sslFactory.configure(serverSslConfig);
+        } catch (Exception e) {
+            fail("An exception was thrown when configuring the truststore without a password: " + e);
+        }
+    }
+
+    @Test
     public void testClientMode() throws Exception {
         File trustStoreFile = File.createTempFile("truststore", ".jks");
         Map<String, Object> clientSslConfig = TestSslUtils.createSslConfig(false, true, Mode.CLIENT, trustStoreFile, "client");
@@ -56,4 +77,42 @@ public class SslFactoryTest {
         assertTrue(engine.getUseClientMode());
     }
 
+    @Test
+    public void testReloadableX509TrustManager() throws Exception {
+        Map<String, X509Certificate> certs = new HashMap<>();
+        String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+        File trustStoreFile = File.createTempFile("truststore", ".jks");
+        Password trustStorePassword = new Password("TrustStorePassword");
+        KeyPair cKP1 = TestSslUtils.generateKeyPair("RSA");
+        X509Certificate cCert1 = TestSslUtils.generateCertificate("CN=localhost, O=client 1", cKP1, 30, "SHA1withRSA");
+        certs.put("client1", cCert1);
+        KeyStore ts = TestSslUtils.createTrustStore(trustStoreFile.getPath(), trustStorePassword, certs);
+        trustStoreFile.deleteOnExit();
+
+        tmf.init(ts);
+        SecurityStore securityStore = new SecurityStore("jks", trustStoreFile.getPath(), trustStorePassword);
+
+        ReloadableX509TrustManager reloadableX509TrustManager = new ReloadableX509TrustManager(securityStore, tmf, 2 * 1000);
+        reloadableX509TrustManager.getAcceptedIssuers();
+
+        // One alias in truststore
+        assertEquals(1, reloadableX509TrustManager.getTrustKeyStore().size());
+
+        KeyPair cKP2 = TestSslUtils.generateKeyPair("RSA");
+        X509Certificate cCert2 = TestSslUtils.generateCertificate("CN=localhost, O=client 2", cKP2, 30, "SHA1withRSA");
+        certs.put("client2", cCert2);
+        TestSslUtils.createTrustStore(trustStoreFile.getPath(), trustStorePassword, certs);
+
+        reloadableX509TrustManager.getAcceptedIssuers();
+
+        // Two aliases in truststore, but not reloaded yet.
+        assertEquals(1, reloadableX509TrustManager.getTrustKeyStore().size());
+
+        Thread.sleep(2 * 1000);
+        reloadableX509TrustManager.getAcceptedIssuers();
+
+        // Two aliases in truststore, should have reloaded.
+        assertEquals(2, reloadableX509TrustManager.getTrustKeyStore().size());
+    }
 }
